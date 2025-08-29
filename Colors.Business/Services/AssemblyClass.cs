995cb87c65850448a4178fa2c6437546bd32fa20
Ben Emanuel <benjaminemanuel13@gmail.com>
2025-08-29 20:53:37 +0100

    Started Assembly Generation (memory map of current Game)

---
+++ Colors.Business/Services/AssemblyClass.cs
@@ -0,0 +1,21 @@
+﻿using Colors.AssemblyModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colors.Business.Services
+{
+    public class AssemblyClass
+    {
+        public void GenerateMemoryMap()
+        {
+            MemoryMap map = new MemoryMap();
+
+            FileStream stream = new FileStream("", FileMode.Create);
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            writer.Write(map.Header.CurrentLevel);
+        }
+}
---
+++ Colors.Common/AssemblyModels/MemoryMapHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colors.AssemblyModels
+{
+    public class MemoryMapHeader
+    {
+        public ushort CurrentLevel { get; set; } = 1;
+    }
+}
