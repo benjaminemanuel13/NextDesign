6d0eaebc48d9c7f144e92d758ceddee3b54f64c5
Ben Emanuel <benjaminemanuel13@gmail.com>
2025-09-09 11:07:33 +0100

    Added Server

---
+++ Colors.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Colors.Server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Colors.Server/Program.cs
@@ -0,0 +1,57 @@
+using Colors.Server.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+var dbUsername = Environment.GetEnvironmentVariable("COLORSUSERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("COLORSPASSWORD");
+var dbIpAddress = Environment.GetEnvironmentVariable("COLORSIPADDRESS");
+var dbName = Environment.GetEnvironmentVariable("COLORSDBNAME");
+
+connectionString = connectionString.Replace("#USERNAME#", dbUsername);
+connectionString = connectionString.Replace("#PASSWORD#", dbPassword);
+connectionString = connectionString.Replace("#IPADDRESS#", dbIpAddress);
+connectionString = connectionString.Replace("#DBNAME#", dbName);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
